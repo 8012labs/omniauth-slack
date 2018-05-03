81babec497f5245be04e9735d15943d0be9f2207
Henry Poydar <henrypoydar@gmail.com>
2018-05-03 16:24:48 -0400

    Remove unused code

--- lib/omniauth/strategies/slack.rb
+++ lib/omniauth/strategies/slack.rb
@@ -106,12 +106,6 @@ module OmniAuth
         scopes = options['scope'].split(',')
         (scopes & bot_scopes).any?
       end
-
-      private
-
-      def callback_url
-        full_host + script_name + callback_path
-      end
     end
   end
 end
