1257243ace0f7574ccffafeb6d01fe8d1fd50d73
Henry Poydar <henrypoydar@gmail.com>
2020-03-26 13:38:42 -0400

    Grab v2 params

--- lib/omniauth/strategies/slack.rb
+++ lib/omniauth/strategies/slack.rb
@@ -19,62 +19,26 @@ module OmniAuth
         param_name: "token",
       }
 
-      uid { raw_info["user_id"] }
+      uid {
+        "#{access_token.params["bot_user_id"]}-#{access_token.params.dig("team", "id")}"
+      }
 
       info do
         hash = {
-          nickname: raw_info["user"],
-          team: raw_info["team"],
-          user: raw_info["user"],
-          team_id: raw_info["team_id"],
-          user_id: raw_info["user_id"],
-        }
-
-        unless skip_info?
-          hash.merge!(
-            name: user_info["user"].to_h["profile"].to_h["real_name_normalized"],
-            email: user_info["user"].to_h["profile"].to_h["email"],
-            first_name: user_info["user"].to_h["profile"].to_h["first_name"],
-            last_name: user_info["user"].to_h["profile"].to_h["last_name"],
-            description: user_info["user"].to_h["profile"].to_h["title"],
-            image_24: user_info["user"].to_h["profile"].to_h["image_24"],
-            image_48: user_info["user"].to_h["profile"].to_h["image_48"],
-            image: user_info["user"].to_h["profile"].to_h["image_192"],
-            team_domain: team_info["team"].to_h["domain"],
-            is_admin: user_info["user"].to_h["is_admin"],
-            is_owner: user_info["user"].to_h["is_owner"],
-            time_zone: user_info["user"].to_h["tz"],
-          )
-        end
-
-        hash
-      end
-
-      extra do
-        hash = {
-          raw_info: raw_info,
-          web_hook_info: web_hook_info,
+          authed_user_info: user_info,
           bot_info: bot_info,
-          params_info: access_token.params,
+          team_info: team_info,
+          web_hook_info: web_hook_info,
         }
-
-        unless skip_info?
-          hash.merge!(
-            user_info: user_info,
-            team_info: team_info,
-          )
-        end
 
         hash
       end
 
-      def raw_info
-        @raw_info ||= access_token.get("/api/auth.test").parsed
-      end
-
-      def user_info
+      def authed_user_info
         url = URI.parse("/api/users.info")
-        url.query = Rack::Utils.build_query(user: raw_info["user_id"])
+        url.query = Rack::Utils.build_query(
+          user: access_tokens.params.dig("authed_user", "id"),
+        )
         url = url.to_s
 
         @user_info ||= access_token.get(url).parsed
@@ -90,8 +54,7 @@ module OmniAuth
       end
 
       def bot_info
-        return {} unless bot_allowed?
-        access_token.params["bot"]
+        { id: access_token.params["bot_user_id"] }
       end
 
       def incoming_webhook_allowed?
@@ -100,15 +63,6 @@ module OmniAuth
         scopes = options["scope"].split(",")
         (scopes & webhooks_scopes).any?
       end
-
-      def bot_allowed?
-        return false unless options["scope"]
-        bot_scopes = ["bot"]
-        scopes = options["scope"].split(",")
-        (scopes & bot_scopes).any?
-      end
-
-      private
 
       def callback_url
         full_host + script_name + callback_path
